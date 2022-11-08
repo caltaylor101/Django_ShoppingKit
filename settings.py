b5cee94a03cce6180e856ac99cb7e4bdf0c54dd2
Cody Llamas <34333250+caltaylor101@users.noreply.github.com>
2022-11-07 22:36:06 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'admin',
-        'PASSWORD': 'secret',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
